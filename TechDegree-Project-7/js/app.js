2edb44666e269e53a0eaf8f55b23cf0addb7c674
AStevens323 <71614086+AStevens323@users.noreply.github.com>
2021-01-28 09:31:01 -0500

    Adding Graphs

--- TechDegree-Project-7/js/app.js
+++ TechDegree-Project-7/js/app.js
@@ -1,4 +1,5 @@
 const alertBanner = document.getElementById("alert");
+const trafficCanvas = document.getElementById("traffic-chart");
 
 // JS Alert Banner --------------------------------------------------------------------------------/
 alertBanner.innerHTML = 
@@ -20,3 +21,42 @@ alertBanner.addEventListener('click', e => {
 });
 
 // END JS Alert Banner --------------------------------------------------------------------------------/
+
+// Chart Widgets -------------------------------------------------------------------------------------/
+    // LINE GRAPH::
+
+    let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+    "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+    2500],
+    backgroundColor: 'rgba(116, 119, 191, .3)',
+    borderWidth: 1,
+    }]
+    };
+
+    let trafficOptions = {
+        aspectRatio: 2.5,
+        animation: {
+        duration: 0
+        },
+        scales: {
+        yAxes: [{
+        ticks: {
+        beginAtZero:true
+        }
+        }]
+        },
+        legend : {
+        display: false
+        }
+        };
+
+        // Adding Chart to HTML
+        let trafficChart = new Chart(trafficCanvas, {
+            type: 'line',
+            data: trafficData,
+            options: trafficOptions
+            });
+    
