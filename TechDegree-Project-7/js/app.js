869526a4f75b9698ddec062721b0fd12bc1239a6
AStevens323 <71614086+AStevens323@users.noreply.github.com>
2021-01-30 10:53:25 -0500

    Alert Banner added to HTML

--- TechDegree-Project-7/js/app.js
+++ TechDegree-Project-7/js/app.js
@@ -1,24 +1,22 @@
-// const alertBanner = document.getElementById("alert");
+const alertBanner = document.getElementById("alert");
 // const trafficCanvas = document.getElementById("traffic-chart");
 
 // // JS Alert Banner --------------------------------------------------------------------------------/
-// alertBanner.innerHTML = 
+alertBanner.innerHTML =
 
-// `
-// <div class="alert-banner">
-// <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
-// to complete</p>
-// <p class="alert-banner-close">x</p>
-// </div>
-// `
-
-// // Event Listener for 'Close' Button
-// alertBanner.addEventListener('click', e => {
-//     const element = e.target;
-//     if (element.classList.contains("alert-banner-close")) {
-//     alert.style.display = "none"
-//     }
-// });
+`
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+to complete</p>
+<p class="alert-banner-close">x</p>
+</div>
+`
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none"
+    }
+});
 
 // // END JS Alert Banner --------------------------------------------------------------------------------/
 
